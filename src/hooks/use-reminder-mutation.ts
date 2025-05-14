eb5a534f16c2cf9500b578a63f773b721fdb01a2
salmansheri <sheriffsalman00@gmail.com>
2025-05-14 18:26:09 +0530

    Small Change

--- src/hooks/use-reminder-mutation.ts
+++ src/hooks/use-reminder-mutation.ts
@@ -30,7 +30,7 @@ export const useReminderMutation = ()=> {
 
             
 
-            console.log({ parseMessageData});
+        
             // console.log({ createReminderData }) 
 
            
